1ba706a1e332ae94fed050001fb54b2740a6562d
Lee Jae Won <jaewon990905@gmail.com>
2020-10-30 17:07:29 +0900

    PlayerController Add

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    
+    private float hAxis;
+    private float vAxis;
+
+    private Vector3 moveVec;
+    
+    private Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+    }
+    
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+    
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+}
