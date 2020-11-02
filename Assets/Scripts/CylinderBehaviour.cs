b13926e4f889a2f10fcd2fa9da67b554f4ef027a
dlwngh1113 <55501976+dlwngh1113@users.noreply.github.com>
2020-11-02 21:59:22 +0900

    obstacles rotation modify

---
+++ Assets/Scripts/CylinderBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationAmount = 0.1f;
+    private float _curRotation;
+    void Awake()
+    {
+        _curRotation = 0f;
+    }
+
+    void Update()
+    {
+        _curRotation += _rotationAmount * Time.deltaTime;
+        Vector3 tmpAngle = gameObject.transform.localRotation.eulerAngles;
+        tmpAngle.x = _curRotation;
+        gameObject.transform.localRotation = Quaternion.Euler(tmpAngle);
+    }
+}
---
+++ Assets/Scripts/PickaxBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+enum RotationDirection
+{
+    PLUS,
+    MINUS
+}
+
+public class PickaxBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationAmount = 0f;
+    private const float _maxRotation = 0.3f;
+    private const float _minRotation = -0.3f;
+    private float _curRotation;
+    private RotationDirection _rotationDirection;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _curRotation = 0f;
+        _rotationDirection = RotationDirection.PLUS;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_rotationDirection)
+        {
+            case RotationDirection.PLUS:
+                _curRotation += _rotationAmount * Time.deltaTime;
+                if (_curRotation > _maxRotation)
+                    _rotationDirection = RotationDirection.MINUS;
+                break;
+            case RotationDirection.MINUS:
+                _curRotation -= _rotationAmount * Time.deltaTime;
+                if (_curRotation < _minRotation)
+                    _rotationDirection = RotationDirection.PLUS;
+                break;
+        }
+        gameObject.transform.Rotate(0, 0, _curRotation);
+        //gameObject.transform.Rotate(gameObject.transform.forward, _curRotation, Space.Self);
+    }
+}
