e27eb1a5274181549aeb9014ee732d458bb9a84b
Lee Jae Won <jaewon990905@gmail.com>
2020-11-20 22:23:28 +0900

    Asset Add, New map, basic AI

---
+++ Assets/Scripts/PushObstacleBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum PushDirection
+{
+    Left,
+    Right
+}
+
+
+public class PushObstacleBehaviour : MonoBehaviour
+{
+    private float start_x = -3.5f;
+    private float end_x = 3.5f;
+    [SerializeField] private float speed;
+    private PushDirection _pushDirection;
+    
+    private Transform _curPosition;
+    void Start()
+    {
+        _curPosition = GetComponent<Transform>();
+        _pushDirection = PushDirection.Right;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        switch (_pushDirection)
+        {
+            case PushDirection.Right:
+                if (_curPosition.transform.position.x > end_x)
+                {
+                    speed *= -1;
+                    _pushDirection = PushDirection.Left;
+                }
+                break;
+            case PushDirection.Left:
+                if (_curPosition.transform.position.x < start_x)
+                {
+                    speed *= -1;
+                    _pushDirection = PushDirection.Right;
+                }
+                break;
+        }
+        gameObject.transform.Translate(speed * Time.deltaTime, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/TestNavMeshAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestNavMeshAgent : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
